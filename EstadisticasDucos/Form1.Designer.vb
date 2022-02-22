4205c55047b8a556b696d48559918410ae944854
Lanthi <lanthi_adam@hotmail.com>
2022-02-22 20:36:45 +0100

    Initial commit

---
+++ EstadisticasDucos/Form1.Designer.vb
@@ -0,0 +1,91 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.txtbalance = New System.Windows.Forms.TextBox()
+        Me.Label35 = New System.Windows.Forms.Label()
+        Me.txtDucoprice = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.SuspendLayout()
+        '
+        'txtbalance
+        '
+        Me.txtbalance.Location = New System.Drawing.Point(60, 12)
+        Me.txtbalance.Name = "txtbalance"
+        Me.txtbalance.ReadOnly = True
+        Me.txtbalance.Size = New System.Drawing.Size(133, 20)
+        Me.txtbalance.TabIndex = 4
+        '
+        'Label35
+        '
+        Me.Label35.AutoSize = True
+        Me.Label35.Location = New System.Drawing.Point(10, 15)
+        Me.Label35.Name = "Label35"
+        Me.Label35.Size = New System.Drawing.Size(49, 13)
+        Me.Label35.TabIndex = 8
+        Me.Label35.Text = "Balance:"
+        '
+        'txtDucoprice
+        '
+        Me.txtDucoprice.Location = New System.Drawing.Point(95, 38)
+        Me.txtDucoprice.Name = "txtDucoprice"
+        Me.txtDucoprice.ReadOnly = True
+        Me.txtDucoprice.Size = New System.Drawing.Size(78, 20)
+        Me.txtDucoprice.TabIndex = 9
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(27, 41)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(62, 13)
+        Me.Label1.TabIndex = 10
+        Me.Label1.Text = "Duco price:"
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 5000
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.txtDucoprice)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtbalance)
+        Me.Controls.Add(Me.Label35)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents txtbalance As TextBox
+    Friend WithEvents Label35 As Label
+    Friend WithEvents txtDucoprice As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Timer1 As Timer
+End Class
---
+++ EstadisticasDucos/Form1.vb
@@ -0,0 +1,35 @@
+﻿Imports System.Net
+Imports System.IO
+Imports System.Web.Script.Serialization
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Actualizar()
+        Timer1.Enabled = True
+    End Sub
+    Private Sub Actualizar()
+        Dim uriString2 As String = "https://server.duinocoin.com/balances/Lanthi"
+        Dim uri2 As New Uri(uriString2)
+        Dim Request2 As HttpWebRequest = HttpWebRequest.Create(uri2)
+        Request2.Method = "GET"
+        Dim Response2 As HttpWebResponse = Request2.GetResponse()
+        Dim Read2 = New StreamReader(Response2.GetResponseStream())
+        Dim Raw2 As String = Read2.ReadToEnd()
+        Dim dict2 As Object = New JavaScriptSerializer().Deserialize(Of Dictionary(Of String, Object))(Raw2)
+        Dim HasesUsuario As Integer = 0
+        ' txtUsuario.Text = dict2.item("result").item("balance").item("username")
+        txtbalance.Text = dict2.item("result").item("balance")
+
+        Dim uriString As String = "https://server.duinocoin.com/statistics"
+        Dim uri As New Uri(uriString)
+        Dim Request As HttpWebRequest = HttpWebRequest.Create(uri)
+        Request.Method = "GET"
+        Dim Response As HttpWebResponse = Request.GetResponse()
+        Dim Read = New StreamReader(Response.GetResponseStream())
+        Dim Raw As String = Read.ReadToEnd()
+        Dim dict As Object = New JavaScriptSerializer().Deserialize(Of Dictionary(Of String, Object))(Raw)
+        txtDucoprice.Text = dict.item("Duco price")
+    End Sub
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        Actualizar()
+    End Sub
+End Class
